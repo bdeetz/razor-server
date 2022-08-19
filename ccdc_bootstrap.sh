5e402e7de5b90cdc74a73efd88af58ac8468ec15
Brady Deetz <bdeetz@gmail.com>
2022-08-19 00:30:55 -0500

    fix path

--- ccdc_bootstrap.sh
+++ ccdc_bootstrap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+cd /var/lib/razor/repo-store
+
 echo "Bootstrapping isos"
 
 curl -k -o ubuntu-16.04.1-server-amd64.iso https://owncloud.tech-hell.com:8444/index.php/s/NdvfibI06WdvTbi/download
